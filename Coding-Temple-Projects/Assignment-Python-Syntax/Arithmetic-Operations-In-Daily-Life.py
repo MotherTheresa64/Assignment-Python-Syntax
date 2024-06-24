497c97334a62a645c798845f0ddc35d794a1fb55
Noah Ragan <64329247+MotherTheresa64@users.noreply.github.com>
2024-06-24 13:13:03 -0500

    Completed Assignment

---
+++ Coding-Temple-Projects/Assignment-Python-Syntax/Arithmetic-Operations-In-Daily-Life.py
@@ -0,0 +1,14 @@
+#Task 1 Grocery Store Math
+peanut_butter = 3.24
+bread = 2.15
+grape_jelly = 3.05
+
+print(grape_jelly + bread + peanut_butter)
+
+#Task 2
+bank_total = 10000
+
+#Interest = P * R * T.
+Interest = bank_total * 0.07 * 1
+
+print(bank_total + Interest)
---
+++ Coding-Temple-Projects/Assignment-Python-Syntax/Python-Naming-Convention-Practice.py
@@ -0,0 +1,8 @@
+#Task 1: Code Correction
+PI = 3.14 #never changes and should be a constant value technically
+user_age = 25
+user_location = "New York"
+MAXLIMIT = 1000
+
+#I know for classes you'd typically capitalize the start of each letter but given that these are variables I chose to 
+#keep them all lowercase except for the "Constant" variables
